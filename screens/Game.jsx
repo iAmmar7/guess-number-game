ed41ac4b3e645d23d091d1ef687cb9fd29a295e5
Ammar Ansari <iammaransari@gmail.com>
2021-10-12 00:32:13 +0500

    game and game over screen optimized with dimensions API

--- screens/Game.jsx
+++ screens/Game.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, StyleSheet, Alert, ScrollView, FlatList } from 'react-native';
+import { View, Text, StyleSheet, Alert, ScrollView, FlatList, Dimensions } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import NumberContainer from '../components/NumberContainer';
@@ -103,13 +103,13 @@ const styles = StyleSheet.create({
   btnContainer: {
     flexDirection: 'row',
     justifyContent: 'space-around',
-    marginTop: 20,
+    marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
     width: 200,
     maxWidth: '80%',
   },
   listContainer: {
     flex: 1,
-    width: '30%',
+    width: Dimensions.get('window').width > 350 ? '30%' : '40%',
     marginTop: 10,
   },
   list: {
--- screens/GameOver.jsx
+++ screens/GameOver.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, Dimensions, ScrollView } from 'react-native';
 
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText';
@@ -8,60 +8,65 @@ import Colors from '../constants/colors';
 
 const GameOver = ({ rounds, userNumber, onNewGame }) => {
   return (
-    <View style={styles.screen}>
-      <TitleText style={styles.title}>The Game is Over!</TitleText>
-      <View style={styles.imageContainer}>
-        <Image
-          // source={require('../assets/success.png')}
-          source={{
-            uri: 'https://www.nicepng.com/png/detail/72-723452_customer-retention-success-person-succes-animated.png',
-          }}
-          style={styles.image}
-          resizeMode="cover"
-        />
+    <ScrollView contentContainerStyle={styles.scrollView}>
+      <View style={styles.screen}>
+        <TitleText style={styles.title}>The Game is Over!</TitleText>
+        <View style={styles.imageContainer}>
+          <Image
+            // source={require('../assets/success.png')}
+            source={{
+              uri: 'https://www.nicepng.com/png/detail/72-723452_customer-retention-success-person-succes-animated.png',
+            }}
+            style={styles.image}
+            resizeMode="cover"
+          />
+        </View>
+        <View style={styles.resultContainer}>
+          <BodyText style={styles.resultText}>
+            I took <Text style={styles.highlight}>{rounds}</Text> rounds to guess your number{' '}
+            <Text style={styles.highlight}>{userNumber}</Text>
+          </BodyText>
+        </View>
+        <MainButton onPress={onNewGame}>NEW GAME</MainButton>
       </View>
-      <View style={styles.resultContainer}>
-        <BodyText style={styles.resultText}>
-          I took <Text style={styles.highlight}>{rounds}</Text> rounds to guess your number{' '}
-          <Text style={styles.highlight}>{userNumber}</Text>
-        </BodyText>
-      </View>
-      <MainButton onPress={onNewGame}>NEW GAME</MainButton>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  screen: {
+  scrollView: {
     flex: 1,
+  },
+  screen: {
+    flexGrow: 1,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: Colors.secondary,
   },
   title: {
-    fontSize: 24,
-    marginVertical: 10,
+    fontSize: Dimensions.get('window').width > 400 ? 24 : 20,
+    marginVertical: Dimensions.get('window').height > 600 ? 10 : 0,
   },
   imageContainer: {
-    width: 300,
-    height: 300,
+    width: Dimensions.get('window').width * 0.7,
+    height: Dimensions.get('window').width * 0.7,
     borderRadius: 150,
     borderWidth: 3,
     borderColor: Colors.white,
     overflow: 'hidden',
-    marginVertical: 30,
+    marginVertical: Dimensions.get('window').height / 40,
   },
   image: {
     width: '100%',
     height: '100%',
   },
   resultContainer: {
-    width: 300,
-    marginBottom: 20,
+    width: Dimensions.get('window').width > 400 ? 300 : 275,
+    marginBottom: Dimensions.get('window').height / 80,
   },
   resultText: {
     textAlign: 'center',
-    fontSize: 18,
+    fontSize: Dimensions.get('window').width > 400 ? 18 : 16,
   },
   highlight: {
     color: Colors.primary,
